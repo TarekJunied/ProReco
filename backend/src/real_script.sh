5364dffe4658c18c60ffdb43e34fa975d4d34142
TarekJunied <77919403+TarekJunied@users.noreply.github.com>
2023-09-12 18:56:27 +0200

    cleaned git status

--- backend/src/real_script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-#SBATCH --ntasks=14
-#SBATCH --time=10:00:00
-#SBATCH --array=0-13
-#SBATCH --mem-per-cpu=5000M
-#SBATCH --job-name=real_logs
-#SBATCH --output=./real_output/output_%a.txt
-echo "Now starting real_logs" 
-/usr/bin/python3.9 recommender.py $SLURM_ARRAY_TASK_ID 14
--- backend/src/training_script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-#SBATCH --ntasks=80
-#SBATCH --time=10:00:00
-#SBATCH --array=0-79
-#SBATCH --mem-per-cpu=5000M
-#SBATCH --job-name=procdisc
-#SBATCH --output=./real_output/output_%a.txt
-echo "Now starting procdisc" 
-/usr/bin/python3.9 training.py $SLURM_ARRAY_TASK_ID 80
