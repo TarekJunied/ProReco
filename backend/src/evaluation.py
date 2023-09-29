7ef74ec4bb3681dcc5ed2840413890be08ed9e6d
TarekJunied <77919403+TarekJunied@users.noreply.github.com>
2023-09-29 09:26:50 +0200

    removed .txt

--- backend/src/evaluation.py
+++ backend/src/evaluation.py
@@ -1,15 +1,11 @@
 import sys
 import sklearn
-from utils import get_all_ready_logs,split_data
+from utils import get_all_ready_logs, split_data
 from recommender import classification
 from filehelper import gather_all_xes
-from measures import read_target_entries,read_target_entry,read_target_vector
+from measures import read_target_entries, read_target_entry, read_target_vector
 from features import read_feature_matrix
 from init import init_testing_logs
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -18,24 +14,24 @@ if __name__ == "__main__":
     testing_logpaths = gather_all_xes("../logs/testing/")
     training_logppaths = gather_all_xes("../logs/training/")
 
-    ready_for_testingpaths = get_all_ready_logs(testing_logpaths,"token_precison")
-    ready_for_trainingpaths = get_all_ready_logs(training_logppaths,"token_precision")
+    ready_for_testingpaths = get_all_ready_logs(
+        testing_logpaths, "token_precison")
+    ready_for_trainingpaths = get_all_ready_logs(
+        training_logppaths, "token_precision")
 
     y_true = [None]*(len(ready_for_testingpaths))
     y_pred = [None]*(len(ready_for_testingpaths))
 
     x_train = read_feature_matrix(ready_for_trainingpaths)
     y_train = read_target_vector(ready_for_trainingpaths, "token_precision")
-    
+
     input(len(ready_for_trainingpaths))
 
     for i in range(len(ready_for_testingpaths)):
-        y_true[i] = read_target_entry(ready_for_testingpaths[i],"token_precision")
-        y_pred[i] = classification(ready_for_testingpaths[i],x_train, y_train)
+        y_true[i] = read_target_entry(
+            ready_for_testingpaths[i], "token_precision")
+        y_pred[i] = classification(ready_for_testingpaths[i], x_train, y_train)
 
-
-
-    print("ACCURACY: ",sklearn.metrics.accuracy_score(y_true,y_pred))
-    print("AVG PRECISION SCORE: ", sklearn.metrics.average_precision_score(y_true, y_pred))
-
-   
+    print("ACCURACY: ", sklearn.metrics.accuracy_score(y_true, y_pred))
+    print("AVG PRECISION SCORE: ",
+          sklearn.metrics.average_precision_score(y_true, y_pred))
