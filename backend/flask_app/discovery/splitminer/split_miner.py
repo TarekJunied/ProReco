514d489fadf910297f66041aea97dc17de66b42c
TarekJunied <77919403+TarekJunied@users.noreply.github.com>
2023-12-25 10:22:54 +0100

    split working now

--- backend/flask_app/discovery/splitminer/split_miner.py
+++ backend/flask_app/discovery/splitminer/split_miner.py
@@ -16,10 +16,8 @@ def discover_petri_net_split(log_path, parallelism_threshold=0.1, frequency_thre
 
     model_path = f"./discovery/models/split_{log_name}"
 
-    command = f"java -cp discovery/splitminer/splitminer.jar:./discovery/splitminer/lib/* au.edu.unimelb.services.ServiceProvider SMPN {parallelism_threshold} {frequency_threshold}\
+    command = f"java -cp \"discovery/splitminer/splitminer.jar:./discovery/splitminer/lib/*\" au.edu.unimelb.services.ServiceProvider SMPN {parallelism_threshold} {frequency_threshold}\
     {remove_or_joins} {log_path} {model_path}"
-
-    input(command)
 
     result = subprocess.run(
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -36,7 +34,6 @@ def discover_petri_net_split(log_path, parallelism_threshold=0.1, frequency_thre
 
     print(f"Success: model stored to {dest}")
 
-    input("hi")
     net, im, fm = pm4py.read.read_pnml(dest)
 
     return net, im, fm
