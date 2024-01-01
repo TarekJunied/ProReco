ce0f65dc873b4d9ec0a304a5830e7f09c73f71f6
TarekJunied <77919403+TarekJunied@users.noreply.github.com>
2024-01-01 21:45:53 +0100

    hi from mac

--- backend/flask_app/first_setup.sh
+++
@@ -1,3 +0,0 @@
-add the following to .zshrc
-export PYTHONPATH=$PYTHONPATH:/root/RecommenderSystem/backend
-and pygraphviz needs you need to include graphviz in the installation command look at documentation
--- frontend/proreco/src/constants/texts.js
+++
@@ -1,8 +0,0 @@
-import aboutText from "./aboutText"
-import tarekText from "./tarekText";
-const texts = {
-    about: aboutText,
-    tarekText: tarekText
-
-}
-export default texts;
